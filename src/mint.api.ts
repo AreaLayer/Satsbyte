f5b3145de137598136a22c86ea01289f8cd42b4d
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-10-07 12:02:47 -0300

    Create mint.api.ts

---
+++ src/mint.api.ts
@@ -0,0 +1,28 @@
+const axios = require('axios').default;
+
+const mintApi = {
+    requestMint: async function (amount) {
+        const { data } = await axios.get(`${MINT_SERVER}/mint`, {
+            params: {
+                amount
+            }
+        })
+        return data
+    },
+    mint: async function (payloads, paymentHash = '') {
+        const { data } = await axios.post(`${MINT_SERVER}/mint`, payloads,
+            {
+                params: {
+                    payment_hash: paymentHash
+                }
+            })
+        return data
+    },
+
+    getKeys: async function () {
+        const { data } = await axios.get(`${MINT_SERVER}/keys`)
+        return data
+    }
+}
+
+module.exports = mintApi
