c31c6aba8486df07c25fba5b6cdd972f69afab10
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-10-07 12:06:24 -0300

    Create wallet.ts

---
+++ src/wallet.ts
@@ -0,0 +1,31 @@
+const { utils } = require('@noble/secp256k1')
+
+function splitAmount(value) {
+    const chunks = []
+    for (let i = 0; i < 32; i++) {
+        const mask = 1 << i
+        if ((value & mask) !== 0) chunks.push(Math.pow(2, i))
+    }
+    return chunks
+}
+
+function bytesToNumber(bytes) {
+    return hexToNumber(utils.bytesToHex(bytes));
+}
+
+function bigIntStringify(key, value) {
+    return typeof value === 'bigint' ? value.toString() : value
+}
+
+function hexToNumber(hex) {
+    if (typeof hex !== 'string') {
+        throw new TypeError('hexToNumber: expected string, got ' + typeof hex);
+    }
+    return BigInt(`0x${hex}`);
+}
+
+module.exports = {
+    splitAmount,
+    bytesToNumber,
+    bigIntStringify
+}
