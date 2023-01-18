a881ece7373637d6f575f0a67501ea38b58b1e06
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-01-18 07:33:30 -0300

    Update mint.api.ts

--- src/mint.api.ts
+++ src/mint.api.ts
@@ -23,6 +23,18 @@ const mintApi = {
         const { data } = await axios.get(`${MINT_SERVER}/keys`)
         return data
     }
+   
 }
 
+const mintFee = {
+       requestFee: async function (fee) {
+           const { data } = await axios.get(' ${FEE_SERVER/mint', {
+                   params: {
+                   fee
+            }
+           }) 
+           return data
+       }
+
 module.exports = mintApi
+module.exports = mintfEE
