da798d4bdab92ba250054613b9907c6fb608ceed
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-03-27 12:08:21 -0300

    Update mint.rs

--- src/operantions/mint.rs
+++ src/operantions/mint.rs
@@ -1,1 +1,20 @@
+use fedimint_core::client_module::{as_any, decoder};
+use fedimint_core::client_module::mint::{self, Mint, Token};
+
+// Define a function that mints Ecash tokens.
+fn mint_ecash(amount: u64) -> Result<Token, Box<dyn std::error::Error>> {
+    // Create a Mint object.
+    let mint = Mint::new();
+
+    // Create a token with the specified amount.
+    let token = mint.mint(Token { amount: amount })?;
+
+    // Serialize the token to a byte array.
+    let token_bytes = token.serialize()?;
+
+    // Deserialize the token from the byte array.
+    let deserialized_token = Token::deserialize(&token_bytes)?;
+
+    Ok(deserialized_token)
+}
 
