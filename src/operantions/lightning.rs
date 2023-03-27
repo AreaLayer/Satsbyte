7181339fbc9d2e7221d266a28f6e6e57473c8d43
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-03-27 12:05:49 -0300

    Update lightning.rs

--- src/operantions/lightning.rs
+++ src/operantions/lightning.rs
@@ -1,1 +1,78 @@
+use bitcoin::util::address::Address;
+use bitcoin::blockdata::transaction::{TxIn, TxOut};
+use bitcoin::network::serialize::BitcoinHash;
+use fedimint_core::core::client::{inputAmount, outputAmount};
+use fedimint_core::core::client::decoder::decoder;
+use fedimint_core::core::client::as_any;
+
+// Define an `Input` struct that wraps a Bitcoin transaction input.
+struct Input {
+    txin: TxIn,
+}
+
+// Implement the `inputAmount` trait for the `Input` struct.
+impl inputAmount for Input {
+    fn amount(&self) -> u64 {
+        self.txin.previous_output.value
+    }
+}
+
+// Define an `Output` struct that wraps a Bitcoin transaction output.
+struct Output {
+    txout: TxOut,
+}
+
+// Implement the `outputAmount` trait for the `Output` struct.
+impl outputAmount for Output {
+    fn amount(&self) -> u64 {
+        self.txout.value
+    }
+}
+
+// Define a function that decodes a Bitcoin transaction from a hex string.
+fn decode_tx(tx_hex: &str) -> Result<bitcoin::Transaction, bitcoin::consensus::encode::Error> {
+    let tx_bytes = hex::decode(tx_hex)?;
+    bitcoin::consensus::deserialize(&tx_bytes)
+}
+
+// Define a function that creates a Bitcoin transaction input from a previous transaction hash and output index.
+fn create_txin(prev_tx_hash: &str, prev_txout_index: u32) -> Result<TxIn, bitcoin::hashes::hex::Error> {
+    let prev_txid = bitcoin::Txid::from_hex(prev_tx_hash)?;
+    let prev_outpoint = bitcoin::OutPoint {
+        txid: prev_txid,
+        vout: prev_txout_index,
+    };
+    Ok(TxIn {
+        previous_output: prev_outpoint,
+        script_sig: bitcoin::Script::new(),
+        sequence: 0,
+        witness: vec![],
+    })
+}
+
+// Define a function that creates a Bitcoin transaction output from an address and amount.
+fn create_txout(address: &str, amount: u64) -> Result<TxOut, bitcoin::util::address::Error> {
+    let addr = Address::from_str(address)?;
+    Ok(TxOut {
+        value: amount,
+        script_pubkey: addr.script_pubkey(),
+    })
+}
+
+// Define a function that converts a `Vec<u8>` to a hex string.
+fn to_hex_string(bytes: &[u8]) -> String {
+    bytes.to_hex()
+}
+
+// Define a function that decodes a transaction from a hex string, extracts its inputs and outputs,
+// and returns a vector of `Input` and `Output` objects.
+fn extract_inputs_outputs(tx_hex: &str) -> Result<(Vec<Box<dyn inputAmount>>, Vec<Box<dyn outputAmount>>), Box<dyn std::error::Error>> {
+    let tx = decode_tx(tx_hex)?;
+    let inputs = tx.input.iter().map(|txin| Box::new(Input { txin: txin.clone() }) as Box<dyn inputAmount>).collect();
+    let outputs = tx.output.iter().map(|txout| Box::new(Output { txout: txout.clone() }) as Box<dyn outputAmount>).collect();
+    Ok((inputs, outputs))
+}
+
+// Define a function that creates a Bitcoin transaction from a list of inputs and outputs.
+fn create_tx(inputs: &[Box<dyn inputAmount>], outputs: &[Box<dyn outputAmount>])
 
